f8d3e7679457bcae05f0c4eedbbde115e3b80f0f
Elias Segura <eliasjsegura00@gmail.com>
2025-04-11 16:44:49 -0600

    feat: i added developers model

---
+++ src/models/DevelopersModel.js
@@ -0,0 +1,42 @@
+import sequelize from "../config/connection.js"
+import { DataTypes } from "sequelize"
+import UsersModel from "./UsersModel.js"
+
+/**
+ * Developers model.
+ * @module DevelopersModel
+ */
+
+const DevelopersModel = sequelize.define(
+    "dev_profile",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        linkedin: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        occupation: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        portfolio: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    },
+    {
+        timestamps: true,
+    }
+)
+
+DevelopersModel.belongsTo(UsersModel, { foreignKey: "user_id" })
+
+export default DevelopersModel

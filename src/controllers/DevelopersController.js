f05fb71205211e6dd4f43b01b713added4ad1d6f
Elias Segura <eliasjsegura00@gmail.com>
2025-04-11 17:06:48 -0600

    feat: i added a function that get a developer by id

--- src/controllers/DevelopersController.js
+++ src/controllers/DevelopersController.js
@@ -1,5 +1,6 @@
 import DevelopersModel from "../models/DevelopersModel.js"
 import UsersModel from "../models/UsersModel.js"
+import RolesModel from "../models/RolesModel.js"
 
 /**
  * create developer
@@ -27,3 +28,53 @@ export const AddNewDeveloper = async (req, res) => {
     }
 }
 
+/**
+ * get developer by id
+ *
+ * function to get developer by id
+ * @param {Object} req - request object
+ * @param {Object} res - response object
+ * @returns {Object} developer retrieved
+ */
+export const DetailsDeveloperId = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        if (!id) throw new Error("Developer id is required")
+
+        const developer = await DevelopersModel.findOne({
+            include: [{
+                model: UsersModel,
+                attributes: [
+                    "role_id",
+                    "name",
+                    "email",
+                    "address",
+                    "phone",
+                    "image",
+                ],
+                where: {
+                    status: 1,
+                },
+                required: true,
+                include: [{
+                    model: RolesModel,
+                    attributes: ["role_name"],
+                    as: "role",
+                }]
+            }],
+            where: {
+                id: id
+            }
+        })
+
+        if (developer) {
+            res.status(200).json({ message: "Developer retrieved successfully", developer })
+        }
+        else {
+            res.status(404).json({ message: "Developer not found" })
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Error retrieving developer", error })
+    }
+}

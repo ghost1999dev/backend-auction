60984f7220df2eb3137bfee3fafe2d4b4c33bb85
Elias Segura <eliasjsegura00@gmail.com>
2025-04-11 17:45:20 -0600

    docs: developers/create documentation

--- src/routes/developerRoutes.js
+++ src/routes/developerRoutes.js
@@ -8,5 +8,28 @@ import {
 
 const router = Router()
 
+/**
+ * @swagger
+ * tags: 
+ *   name: Developers
+ *   description: Operations about developers 
+ */
 
-
+/**
+ * @swagger
+ * /developers/create:
+ *  post:
+ *    tags: [Developers]
+ *    summary: Create a new developer
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/developerCreate'
+ *    responses:
+ *      200:
+ *        description: Returns a new developer
+ *      500:
+ *        description: Server error
+ */

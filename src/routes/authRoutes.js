a3495b407cf5fb4ef9fb752ddfd5063459245104
Manasés Turcios Ortéz <manasesortez1234@hotmail.com>
2025-04-12 19:51:08 -0600

    :feat authRoutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,6 +2,10 @@ import { Router } from "express";
 
 const loginRouter = Router();
 
-loginRouter.get("/google", (req, res) => res.send(req.user));
+loginRouter.get("/google", (req, res) => {
+    res.redirect(`http://localhost:4200/#/main/dashboard`);
+    //res.send(req.user)
+}
+);
 
 export { loginRouter };

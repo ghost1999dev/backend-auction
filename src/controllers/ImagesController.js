046c10f4d4fd0bdd69066d903aeb5421e35811ea
Elias Segura <eliasjsegura00@gmail.com>
2025-04-30 12:24:30 -0600

    refactor: changes on imagecontroller

--- src/controllers/ImagesController.js
+++ src/controllers/ImagesController.js
@@ -1,3 +1,40 @@
+import multer from "multer"
+import path from "path"
+import { fileURLToPath } from "url"
+import uploadImage from "../helpers/uploadImage.js"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../images/temp"))
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`)
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /jpeg|jpg|png|webp/;
+  const extmane = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = allowedTypes.test(file.mimetype);
+
+  if (mimetype && extmane) {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid file type"));
+  }
+}
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: {
+    fileSize: 1024 * 1024 * 10
+  }
+})
+
 /**
  * update image
  *
@@ -10,14 +47,51 @@
 const updateImage = async (req, res, model) => {
   try {
     const { id } = req.params;
-    let imagePath = req.file.path;
 
-    imagePath = imagePath.replace(/\\/g, "/");
+    if (!id) {
+      return res
+        .status(400)
+        .json({
+          status: 400,
+          message: "No id provided" 
+        });
+    }
 
-    await model.update({ image: imagePath }, { where: { id: id } });
-    res.status(200).json({ message: "Image updated successfully" });
+    if (!req.files || !req.files.length === 0) {
+      return res
+        .status(400)
+        .json({
+          status: 400,
+          message: "No file uploaded" 
+        });
+    }
+
+    const uploadPromises = uploadImage({
+      filePath: req.files[0].path,
+      originalName: req.files[0].originalname,
+      folderName: "images/"
+    }) 
+
+    const result = await Promise.all(uploadPromises)
+
+    //imagePath = imagePath.replace(/\\/g, "/");
+
+    await model.update({ image: result.url }, { where: { id: id } });
+    
+    res
+      .status(200)
+      .json({ 
+        status: 200,
+        message: "Image updated successfully",
+        image: result.url
+      });
   } catch (error) {
-    res.status(500).json({ message: "Error updating image", error });
+    res
+      .status(500)
+      .json({ 
+        status: 500,
+        message: "Error updloading image", error 
+      });
   }
 };
 

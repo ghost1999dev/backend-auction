01a4299274cc5cf54784eadd94f8197e27ff740e
Manasés Turcios Ortéz <manasesortez1234@hotmail.com>
2025-05-01 11:27:10 -0600

    :feat add feutures

--- src/validations/usersSchema.js
+++ src/validations/usersSchema.js
@@ -14,30 +14,10 @@ export const createUserSchema = Joi.object({
   phone: Joi.string()
   .pattern(/^\+\(\d{3}\) \d{4}-\d{4}$/)
   .required()
-  .error((errors) => {
-    return errors.map((error) => {
-      switch (error.code) {
-        case 'string.pattern.base':
-          return new Error(JSON.stringify({
-            success: false,
-            message: "The phone format must be +(XXX) XXXX-XXXX",
-            status: 400
-          }));
-        case 'string.empty':
-          return new Error(JSON.stringify({
-            success: false,
-            message: "The phone number cannot be empty",
-            status: 400
-          }));
-        default:
-          return new Error(JSON.stringify({
-            success: false,
-            message: "Phone validation error",
-            status: 400
-          }));
-      }
-    });
-  }),
+    .messages({
+      'string.pattern.base': 'Phone numbers format is wrong',
+      'string.empty': 'Number is empty'
+    }),
   image: Joi.string().optional().allow(null, ''),
   account_type: Joi.number().required()
 });
@@ -48,65 +28,20 @@ export const updateUserSchema = Joi.object({
     phone: Joi.string()
     .pattern(/^\+\(\d{3}\) \d{4}-\d{4}$/)
     .required()
-    .error((errors) => {
-      return errors.map((error) => {
-        switch (error.code) {
-          case 'string.pattern.base':
-            return new Error(JSON.stringify({
-              success: false,
-              message: "Phone numbers format is wrong",
-              status: 400
-            }));
-          case 'string.empty':
-            return new Error(JSON.stringify({
-              success: false,
-              message: "Number is empty",
-              status: 400
-            }));
-          default:
-            return new Error(JSON.stringify({
-              success: false,
-              message: "Number validation error",
-              status: 400
-            }));
-        }
-      });
+    .messages({
+      'string.pattern.base': 'Phone numbers format is wrong',
+      'string.empty': 'Number is empty'
     })
 });
 
 export const passwordUserchema = Joi.object({
     currentPassword: Joi.string().optional().allow('',null),
     Newpassword: Joi.string()
-      .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/)
+      .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$/)
       .required()
-      .error((errors) => {
-        return errors.map((error) => {
-          switch (error.code) {
-            case 'string.pattern.base':
-              return new Error(JSON.stringify({
-                success: false,
-                message: "The new password must have at least one uppercase letter, one lowercase letter, one number, and at least 8 alphanumeric characters.",
-                status: 400
-              }));
-            case 'string.empty':
-              return new Error(JSON.stringify({
-                success: false,
-                message: "The new password cannot be empty.",
-                status: 400
-              }));
-            case 'any.required':
-              return new Error(JSON.stringify({
-                success: false,
-                message: "The new password is mandatory.",
-                status: 400
-              }));
-            default:
-              return new Error(JSON.stringify({
-                success: false,
-                message: "Password validation error",
-                status: 400
-              }));
-          }
-        });
+      .messages({
+        'string.pattern.base': 'La nueva contraseña debe tener al menos una mayúscula, una minúscula, un número y mínimo 6 caracteres alfanuméricos.',
+        'string.empty': 'La nueva contraseña no puede estar vacía.',
+        'any.required': 'La nueva contraseña es obligatoria.'
       })
   });

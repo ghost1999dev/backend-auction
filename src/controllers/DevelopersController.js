e47ea0c1da2af54ad0983c2e7f76cf4dede85fb9
Elias Segura <eliasjsegura00@gmail.com>
2025-05-01 14:34:31 -0600

    refac: getDeveloperbyUserId

--- src/controllers/DevelopersController.js
+++ src/controllers/DevelopersController.js
@@ -1,7 +1,6 @@
 import DevelopersModel from "../models/DevelopersModel.js"
 import UsersModel from "../models/UsersModel.js"
 import RolesModel from "../models/RolesModel.js"
-import { stat } from "fs"
 import { GetObjectCommand } from "@aws-sdk/client-s3"
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 import { s3Client } from "../utils/s3Client.js"
@@ -178,11 +177,35 @@ export const getDevelopersByIdUser = async (req, res) => {
         })
 
         if (developer) {
+
+            let imageUrl = ''
+            if (developer.user.image){
+                const s3key = developer.user.image.includes("amazonaws.com/") 
+                ? developer.user.image.split("amazonaws.com/")[1]
+                : developer.user.image
+
+                const command = new GetObjectCommand({
+                    Bucket: process.env.BUCKET_NAME,
+                    Key: s3key,
+                })
+
+                imageUrl = await getSignedUrl(s3Client, command, { expiresIn: 60 * 60 * 24 })
+            }
+
+            const developerWithImage = {
+                ...developer.dataValues,
+                user: {
+                    ...developer.user.dataValues,
+                    image: imageUrl
+                }
+            }
+
             res
                 .status(200)
                 .json({ 
                     status: 200,
-                    message: "Developer retrieved successfully", developer 
+                    message: "Developer retrieved successfully", 
+                    developer: developerWithImage
                 })
         }
         else {

303d512d4895734cdc913ae09d9f30263926e3be
andersonramirez15 <rmluis968@gmail.com>
2025-05-22 11:14:38 -0600

    Create RatingModel

---
+++ src/models/RatingModel.js
@@ -0,0 +1,60 @@
+import sequelize from "../config/connection.js";
+import { DataTypes } from "sequelize";
+import UsersModel from "./UsersModel.js";
+
+/**
+ * Rating model.
+ * @module RatingModel
+ */ 
+
+  const RatingModel = sequelize.define(
+    "rating", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    developer_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    company_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    score: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5
+      }
+    },
+    comment: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    isVisible: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    },
+  }, {
+    timestamps: true
+  });  
+
+  // Asociaciones
+  RatingModel.associate = (models) => {
+    Rating.belongsTo(models.UsersModel, {
+      foreignKey: 'developer_id',
+      as: 'developer'
+    });
+
+    RatingModel.belongsTo(models.UsersModel, {
+      foreignKey: 'company_id',
+      as: 'company'
+    });
+  };
+ 
+  export default RatingModel;
+
+

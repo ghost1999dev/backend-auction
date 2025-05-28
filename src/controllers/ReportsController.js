1ca6ea602e1b308f4ccd47631074ba38501416aa
andersonramirez15 <rmluis968@gmail.com>
2025-05-28 09:02:58 -0600

    Create ReportsController

---
+++ src/controllers/ReportsController.js
@@ -0,0 +1,11 @@
+import { Op } from 'sequelize';
+import { Sequelize } from 'sequelize';
+import ReportsModel from '../models/ReportsModel.js';
+import UsersModel from '../models/UsersModel.js';
+import ProjectsModel from '../models/ProjectsModel.js';
+
+/**
+ * Reports controller.
+ * @module ReportsController
+ */
+

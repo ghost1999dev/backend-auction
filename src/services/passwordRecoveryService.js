979128d4ab39afaf9b2ec59f1fcb57963ee8b500
andersonramirez15 <rmluis968@gmail.com>
2025-06-11 09:31:20 -0600

    updaye funcion requestPasswordRecovery

--- src/services/passwordRecoveryService.js
+++ src/services/passwordRecoveryService.js
@@ -1,32 +1,27 @@
 import { emailVerificationService } from "../helpers/emailVerification.js"
 
 export const requestPasswordRecovery = async (email) => {
-    try {
-  
-      const response = await emailVerificationService(email);
-  
-      if (response.status === 200) {
-        res
-          .status(200)
-          .json({
-            status: 200,
-            message: "Codigo de recuperacion enviado."
-          })
-      }
-      else {
-        res
-          .status(response.status)
-          .json({
-            status: response.status,
-            message: "Correo no encontrado"
-          })
-      }
-    } catch (error) {
-      res
-        .status(500)
-        .json({ 
-          status: 500,
-          message: "Error al enviar el correo de recuperacion", error: error.message 
-        })
+  try {
+    const response = await emailVerificationService(email);
+
+    if (response.status === 200) {
+      return {
+        status: 200,
+        message: "Código de recuperación enviado.",
+        code: response.code 
+      };
+    } else {
+      return {
+        status: response.status,
+        message: "Correo no encontrado"
+      };
     }
-}
+  } catch (error) {
+    return {
+      status: 500,
+      message: "Error al enviar el correo de recuperación",
+      error: error.message
+    };
+  }
+};
+

91eb859778f53b23414e8de43d91e8806f826ca3
Elias Segura <eliasjsegura00@gmail.com>
2025-06-20 14:44:29 -0600

    feat: agregue un servicio para bloquear usuarios

---
+++ src/services/nodecronService.js
@@ -0,0 +1,58 @@
+import cron from 'node-cron'
+import { Op } from 'sequelize'
+import CompaniesModel from '../models/CompaniesModel.js'
+import ProjectsModel from '../models/ProjectsModel.js'
+import UsersModel from '../models/UsersModel.js'
+import NotificationsModel from '../models/NotificationsModel.js'
+import sequelize from '../config/connection'
+
+cron.schedule('0 0 * * *', async () => {
+    try {
+        const finishedProjects = await ProjectsModel.findAll({
+            where: { 
+                status: 4,
+                [Op.and]: [
+                    sequelize.where(
+                        sequelize.fn('AGE', sequelize.col('updatedAt')),
+                        { [Op.gte]: sequelize.literal("INTERVAL '1 days'") }
+                    )
+                ],
+            },
+            include: [{ 
+                model: CompaniesModel, 
+                as: 'company',
+                include: [{
+                    model: UsersModel
+                }] 
+            }]
+        })
+
+        for (const project of finishedProjects) {
+            await UsersModel.update({
+                status: 5,
+                where: {
+                    id: project.company.user_id
+                }
+            })
+
+            await NotificationsModel.create({
+                user_id: project.company.user_id,
+                title: 'Usuario bloqueado',
+                body: `El usuario ${project.company.name} ha sido bloqueado porque no ha programado la subasta en el tiempo disponible.`,
+                context: JSON.stringify({
+                    action: 'user_auto_block',
+                    user_id: project.company.user_id,
+                    status: 'bloqueado'
+                }),
+                sent_at: new Date(),
+                status: 'Enviado',
+                error_message: null
+            })
+        }
+    } catch (error) {
+        console.error({
+            message: 'Error en la verificación diaria de proyectos:',
+            error: error.message
+        })
+    }
+})

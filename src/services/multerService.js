1ce5038ad3b9db7d1b4f42195f170eba9abab2dd
Elias Segura <eliasjsegura00@gmail.com>
2025-07-02 13:57:26 -0600

    feat: agregue servicio de multer para archivos

--- src/services/multerService.js
+++ src/services/multerService.js
@@ -2,6 +2,8 @@ import multer from 'multer'
 import multerS3 from 'multer-s3'
 import { s3Client } from '../utils/s3Client.js'
 
+const allowedExtensions = ['.pdf', '.jpg', '.jpeg', '.png', '.docx']
+
 const uploadDocuments = multer({
     storage: multerS3({
         s3: s3Client,
@@ -14,8 +16,16 @@ const uploadDocuments = multer({
             cb(null, `${Date.now()}-${file.originalname}`)
         }
     }),
+    fileFilter: function (req, file, cb) {
+        const ext = path.extname(file.originalname).toLowerCase()
+        if (allowedExtensions.includes(ext)) {
+            cb(null, true);
+        } else {
+            cb(new Error(`Extensión no permitida. Solo se aceptan: ${allowedExtensions.join(', ')}`), false);
+        }
+    },
     limits: {
-        fileSize: 1024 * 1024 * 30,
+        fileSize: 1024 * 1024 * 20,
     },
 }).array('files', 10)
 

4581b994633199d948d7e6b72ca31b0206566403
andersonramirez15 <rmluis968@gmail.com>
2025-08-13 15:29:47 -0600

    Creating model ProjectTrackingModel for follow-up

---
+++ src/models/ProjectTrackingModel.js
@@ -0,0 +1,41 @@
+import sequelize from "../config/connection.js";
+import { DataTypes } from "sequelize";
+import ProjectsModel from "./ProjectsModel.js";
+
+const ProjectTrackingModel = sequelize.define('project_tracking', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  project_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'projects',
+      key: 'id'
+    }
+  },
+  status: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    comment: "1 = Asignado, 2 = En Progreso, 3 = En Revisión, 4 = Completado"
+  },
+  notes: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  timestamps: false
+});
+
+ProjectTrackingModel.belongsTo(ProjectsModel, {
+  foreignKey: 'project_id',
+  as: 'project'
+});
+
+export default ProjectTrackingModel;

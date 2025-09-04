5498b2d6fa664fe7923ec22b95921899d14dc01b
fernandoDev <blancogonzalezfernandoalexis@gmail.com>
2025-09-04 10:51:00 -0600

    feature:(apiBidsService) i added some log from microservice number 2

--- src/services/apiBidsService.js
+++ src/services/apiBidsService.js
@@ -2,6 +2,8 @@ const BASE_URL = process.env.BIDS_BASE_URL
 const API_KEY = process.env.BIDS_API_KEY
 
 async function createBidApi(bid){
+    console.log(`PAYLOAD BID ${bid}`);
+    
     const res = await fetch(`${BASE_URL}/api/bids`, {
         method: 'POST',
         headers: {
@@ -10,11 +12,9 @@ async function createBidApi(bid){
         },
         body: JSON.stringify(bid)
     })
-    console.log('RESULT FROM CREATEDBID');
-    
-    console.log(`RESULT ${JSON.stringify(res)}`);
-    
+    console.log('*******RESULT FROM CREATEDBID******');
     const json = await res.json()
+    console.log('RESPONSE FROM MICROSERVICE:', json);
     if (!res.ok || !json.success) throw new Error(json.message || `HTTP ${res.status}`)
     return json.data
 }
